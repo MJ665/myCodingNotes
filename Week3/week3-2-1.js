eadfed4ae7131b2d386661753ff1ac08960bb3cf
Meet Jain <meetj665@gmail.com>
2024-03-31 19:29:14 +0530

    week 3 2 1 .js

---
+++ Week3/week3-2-1.js
@@ -0,0 +1,31 @@
+/**
+    * * Highlight: in this thing we will study fetch api and authentication and mongodb
+    * ! Important: Cyrptography Jarong  in  authenticaiton are
+    * ! Hashing
+1. Hashing is one way 
+2. Given the output, no one can find out the input 
+3. Changing the input a lil bit changes the output by a lot
+
+    * ! Encryption
+1. Encryption is two way 
+2. A string is encrypted using a password 
+3. String can be decrypted using the same password 
+
+    * ! Json web tokens
+1. Its neither of encryption or hashing  
+(its technically a digital signature) 
+2. Anyone can see the original output given the signature 
+3. Signature can be verified only using the password
+Password
+4. It takes json as an input it works in web and in 
+
+    * ! Local Storage
+A place in your browser where you can store some data 
+Usually things that are stored include -  
+1. Authentication tokens 
+2. User language preference 
+3. User theme preference
+
+    * ? Question: 
+    * TODO todo: 
+**/
