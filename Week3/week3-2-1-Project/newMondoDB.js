6083fbf103e318a1612b7b0be7b71f70fc688d56
Meet Jain <meetj665@gmail.com>
2024-04-01 21:45:48 +0530

    code not working newMondoDB week 3 2 1 project

--- Week3/week3-2-1-Project/newMondoDB.js
+++ Week3/week3-2-1-Project/newMondoDB.js
@@ -1,88 +1,89 @@
-const mongoose= require("mongoose")
-const express = require ("express")
-const app = express()
-const jwtPassword = password
+const mongoose = require("mongoose");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const jwtPassword = "password";
+const cors = require("cors");
+// const fetch = require("node-fetch"); // Added for making HTTP requests
+app.use(cors());
 
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+  });
+  
+try{
+// MongoDB connection
+mongoose.connect("mongodb+srv://hackathonmj641:1029384756@cluster0.bomrqhm.mongodb.net/mydatabase");
+}catch(err){
+    console.log(err)
+}
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String
+});
 
-app.use(express.json())
-
-mongoose.connect("mongodb+srv://hackathonmj641:1029384756@cluster0.bomrqhm.mongodb.net/")
-const mongoModel= mongoose.model("User",{
-    name:string,
-    username:string,
-    password:string
-})
-
-app.post("/signUp",(req,res)=>{
-    if (userExists(req.body.email)){
-        res.status(400).json("user already exists")
-    }
-const user = new User ({
-    name : req.body.name,
-    email : req.body.email,
-    password : req.body.password
-})
-user.save()
-res.json({
-    msg:"User successfully created"
-})
-})
-
-
-user.save()
-
-async function  userExists(username,password){
-    const existingUser= await User.findOne({email:username})
-if (existingUser){return true}else{return false}
+// Function to check if user exists
+async function userExists(email) {
+  const existingUser = await User.findOne({ email: email });
+  return !!existingUser;
 }
 
-app.post ("/signin",(req,res)=>{
-    const username=req.body.username
-    const password = req.body.password
-    if(!userExists){
-        return res.status(403).json({
-            "error": "user not exits",
-            
-        })
+// Sign up route
+app.post('/signUp', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (await userExists(email)) {
+      return res.status(400).json({ error: "User already exists" });
     }
-    var token = jwt.sign({username:username},jwtPassword)
-    return res.json({
-        userSignedIN:True,
-        token : token
-    })
-})
 
-app.get("/users",(req,res)=>{
-    const token = req.header.authorization
-    try { 
-        const decode = jwt.verify(token,jwtPassword)
-        const userName = decode.username
+    const user = new User({ name, email, password });
+    await user.save();
+    res.json({ msg: "User successfully created" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error", errorMsg: error.message });
+  }
+});
 
-        if(!userExists(userName)){
-            throw "user not exist"
-        }
-        else{return userList}
-    }catch(err){
-        return res.status(400).json({
-            error:"error",
-            msg:err
-        })
+// Sign in route
+app.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(401).json({ error: "User not found" });
     }
-})
 
+    // Perform password validation here
 
+    const token = jwt.sign({ email: existingUser.email }, jwtPassword);
+    res.json({ userSignedIn: true, token });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error", errorMsg: error.message });
+  }
+});
 
+// Users route
+app.get("/users", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, jwtPassword);
+    const userEmail = decoded.email;
 
+    if (!(await userExists(userEmail))) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
+    const userList = await User.find({}, "-password");
+    res.json(userList);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error", errorMsg: error.message });
+  }
+});
 
-
-
-
-app.use((err,req,res,next)=>{
-    res.json({
-        "error":500,
-        "errorMsg":err
-    })
-})
-
-app.listen(3000, ()=>{console.log("we are listening on port 3000")})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
