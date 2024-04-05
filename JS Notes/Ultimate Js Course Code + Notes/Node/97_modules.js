ff324d81dd7c2e0da07cbae54603f662b37c2e40
Meet Jain <meetj665@gmail.com>
2024-04-05 22:16:30 +0530

    new branch

--- JS Notes/Ultimate Js Course Code + Notes/Node/97_modules.js
+++
@@ -1,8 +0,0 @@
-// const {hello, ahello} = require("./module1") // commonjs
-import harry, {hello, ahello} from "./module2.js";
-hello()
-ahello("Shivani")
-ahello("Garima")
-ahello("Nitika")
-ahello("Aishwarya")
-harry()
